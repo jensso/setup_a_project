51ac8fe3d3298820591a20a6464de754aa808058
jens soltwedel <jens.soltwedel@hotmail.com>
2019-01-09 12:47:54 +0100

    2 tests added

--- scripts/setup.js
+++ scripts/setup.js
@@ -1,1 +1,19 @@
-console.log(5);
+let convertToNr = (x)=> {
+   parseInt(x);
+   return x;
+};
+
+let makePallindrom = (str)=> {
+
+  if (typeof str !=='string') {
+  return false;
+  }
+  return str.split('').reverse().join('');
+
+
+};
+
+
+module.exports = {convertToNr: convertToNr,
+                  makePallindrom: makePallindrom,
+                }
--- test/test.js
+++ test/test.js
@@ -1,8 +1,28 @@
+const factory = require('../scripts/setup.js');
 const assert = require('assert');
 
 describe('my suite', ()=> {
+  describe('test string convert to nr', ()=> {
 
+    it('should convert a string to a number', ()=> {
+      assert.equal(factory.convertToNr('5'),5);
+    })
+  })
+describe('test make pallindrom', ()=> {
+  it('should reverse the string which was passed', ()=> {
+    const actualResult = factory.makePallindrom('jake');
+    const expectedResult = 'ekaj';
 
+    assert.strictEqual(actualResult,expectedResult);
 
+  })
+  it('should return false if arg isnt a string', ()=> {
+    const actualResult = factory.makePallindrom(75);
+    const expectedResult = false;
 
+    assert.strictEqual(actualResult,expectedResult);
+
+  })
 })
+
+}) // suite
